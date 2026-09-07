634df8f8316c8c435e8e0bbd969063d3bf79b012
noreplayeliotel-hash <noreplayeliotel@gmail.com>
2026-09-07 12:11:04 +0100

    Update upload.js

--- services/upload.js
+++ services/upload.js
@@ -8,7 +8,7 @@ const FTP_HOST = process.env.FTP_HOST;
 const FTP_USER = process.env.FTP_USER;
 const FTP_PASSWORD = process.env.FTP_PASSWORD;
 const FTP_DIR = 'upload';  // Conservé comme dans le code original
-const BASE_URL = 'https://m4ckwwswggwo8c8g08gwsscc.duckdns.org/afficheimage/image';  // Conservé comme dans le code original
+const BASE_URL = 'https://image.eliotel.com/afficheimage/image';  // Conservé comme dans le code original
 
 /**
  * Fonction pour télécharger un fichier avec réessais automatiques
